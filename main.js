e0d238aa92ebc6a224437d696c70f2d279bebb20
AndyLampert <lamperta@gmail.com>
2014-04-16 17:34:50 -0600

    finished exercise, included auto resizing marker, remove marker functionality

--- main.js
+++ main.js
@@ -1,22 +1,15 @@
 $(document).on('ready', function() {
-    $('.topo').on('click', function(){
-        $('.marker')
-            .css("display", "block")
-            .offset({
-                top: event.pageY,
-                left: event.pageX
-            });
+    $('.topo').on('click', function() {
+        var halfOfWidth = $('.marker').width() / 2;
+        var halfOfHeight = $('.marker').height() / 2;
+        $('.marker').css({
+            "display": "block"
+        }).offset({
+            top: event.pageY - halfOfWidth,
+            left: event.pageX - halfOfHeight
+        });
+        $(document).on('click', '.marker', function() {
+            $(this).hide();
+        })
     });
 });
-
-// use the .css to position the marker div
-// make sure we're not applying the .offset to every .marker
-// but instead just on the one we create when we click
-
-// Goal 1 
-// when click, add a .marker div somewhere (just have it show up)
-// how to test selectors to make sure they aren't wrong / work as intended
-// sometimes elements just might not be there yet 
-// if the element isn't there yet, jquery won't report an error
-// console log the $('selector').length
-// 
